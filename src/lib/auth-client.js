77ba3cd0937b4cbe576eb1c39da93b8bc3fab571
Sami Abdullah <sami38596@gmailcom>
2026-07-10 20:40:35 +0600

    added

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,8 @@
-import { adminClient, emailOTPClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
+import { adminClient, emailOTPClient } from "better-auth/client/plugins";
+
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: `${process.env.NEXT_PUBLIC_APP_URL}${process.env.NEXT_PUBLIC_API_URL}`,
   plugins: [adminClient(), emailOTPClient()],
 });
 
